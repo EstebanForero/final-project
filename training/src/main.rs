de978641a5c3e4d69e8da3a76032d50a498487d4
Esteban Forero <estebanmff@outlook.com>
2026-05-18 18:04:54 -0500

    better qvalues

--- training/src/main.rs
+++ training/src/main.rs
@@ -1,6 +1,9 @@
 use std::thread::JoinHandle;
 
+use rayon::prelude::*;
+
 use training::trial_gen::ProjectedTrial;
+use training::types::{QValues, Transition};
 use training::{
     policy_evaluator::{FirstVisitMonteCarloEvaluator, PolicyTrialEvaluator},
     policy_improver::{GreedyPolicy, UcbPolicy},
@@ -8,31 +11,53 @@ use training::{
     trial_gen::{MonteCarloTreeSearch, OnlinePolicyImprovementTrialGenerator, TrialGenerator},
 };
 
+const BATCH_SIZE: usize = 8;
+
+fn make_trial_generator(
+    q_values: QValues,
+) -> OnlinePolicyImprovementTrialGenerator<UcbPolicy, GreedyPolicy> {
+    let tree_policy = UcbPolicy::new(1.4);
+    let rollout_policy = GreedyPolicy::new();
+
+    let montecarlo_tree_search = MonteCarloTreeSearch::new(
+        tree_policy,
+        rollout_policy,
+        500,
+    );
+
+    OnlinePolicyImprovementTrialGenerator::new(
+        montecarlo_tree_search,
+        q_values,
+    )
+}
+
 fn main() {
     let q_value_persistence = QValuePersistence::new("./q_values".into());
 
     let mut global_q_values = q_value_persistence.load_q_values();
-
-    let tree_policy = UcbPolicy::new(1.4);
-    let rollout_policy = GreedyPolicy::new();
-
-    let montecarlo_tree_search = MonteCarloTreeSearch::new(tree_policy, rollout_policy, 100);
-
-    let mut trial_generator =
-        OnlinePolicyImprovementTrialGenerator::new(montecarlo_tree_search, global_q_values.clone());
 
     let policy_evaluator = FirstVisitMonteCarloEvaluator::new(1.0);
 
     let mut save_handle: Option<JoinHandle<()>> = None;
 
     for iteration in 0..100_000 {
-        trial_generator.set_q_values(global_q_values.clone());
+        let q_snapshot = global_q_values.clone();
 
-        let trial = trial_generator.generate_trial();
-        let (trial_player_a, trial_player_b) = trial.project_for_players();
+        let projected_trials: Vec<(Vec<Transition>, Vec<Transition>)> =
+            (0..BATCH_SIZE)
+                .into_par_iter().map(|_| {
+                let mut trial_generator = make_trial_generator(q_snapshot.clone());
 
-        policy_evaluator.evaluate_trial(&trial_player_a, &mut global_q_values);
-        policy_evaluator.evaluate_trial(&trial_player_b, &mut global_q_values);
+                trial_generator.generate_trial()
+                .project_for_players()
+            }).collect();
+
+
+        for (trial_player_a, trial_player_b) in projected_trials {
+            policy_evaluator.evaluate_trial(&trial_player_a, &mut global_q_values);
+            policy_evaluator.evaluate_trial(&trial_player_b, &mut global_q_values);
+        }
+        
 
         if iteration % 1000 == 0 && iteration != 0 {
             println!(
